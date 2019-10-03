d6fa377ab4fbeab6078a07d4664d025d14fb9fac
SDK Automation <sdkautomation@microsoft.com>
2019-10-03 15:07:07 +0000

    Generated from dcfa37da701230f1146aca1f33e660a448c6ef43
    additional change

--- network/resource-manager/v2018_07_01/src/main/java/com/microsoft/azure/management/network/v2018_07_01/OutboundRule.java
+++ network/resource-manager/v2018_07_01/src/main/java/com/microsoft/azure/management/network/v2018_07_01/OutboundRule.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.microsoft.rest.serializer.JsonFlatten;
 
 /**
- * Outbound pool of the load balancer.
+ * Outbound rule of the load balancer.
  */
 @JsonFlatten
 public class OutboundRule extends SubResource {
