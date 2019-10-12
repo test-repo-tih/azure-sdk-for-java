46aeeb58f4a419621f21c4fe821cf0752e3aba48
Neeraj Makam <neeraj.makam@microsoft.com>
2019-10-11 17:05:59 -0700

    Disabling DTD (#5706)
    XML Serialization had security issues regarding DTD. Disabling those features which are not required.
    Fixes #4466 .

--- sdk/storage/azure-storage-file/src/main/java/com/azure/storage/file/implementation/SignedIdentifiersWrapper.java
+++ sdk/storage/azure-storage-file/src/main/java/com/azure/storage/file/implementation/SignedIdentifiersWrapper.java
@@ -4,7 +4,7 @@
 
 package com.azure.storage.file.implementation;
 
-import com.azure.storage.file.models.FileSignedIdentifier;
+import com.azure.storage.file.models.SignedIdentifier;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -12,12 +12,12 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import java.util.List;
 
 /**
- * A wrapper around List&lt;FileSignedIdentifier&gt; which provides top-level metadata for serialization.
+ * A wrapper around List&lt;SignedIdentifier&gt; which provides top-level metadata for serialization.
  */
 @JacksonXmlRootElement(localName = "SignedIdentifiers")
 public final class SignedIdentifiersWrapper {
     @JacksonXmlProperty(localName = "SignedIdentifier")
-    private final List<FileSignedIdentifier> signedIdentifiers;
+    private final List<SignedIdentifier> signedIdentifiers;
 
     /**
      * Creates an instance of SignedIdentifiersWrapper.
@@ -25,16 +25,16 @@ public final class SignedIdentifiersWrapper {
      * @param signedIdentifiers the list.
      */
     @JsonCreator
-    public SignedIdentifiersWrapper(@JsonProperty("SignedIdentifier") List<FileSignedIdentifier> signedIdentifiers) {
+    public SignedIdentifiersWrapper(@JsonProperty("SignedIdentifier") List<SignedIdentifier> signedIdentifiers) {
         this.signedIdentifiers = signedIdentifiers;
     }
 
     /**
-     * Get the List&lt;FileSignedIdentifier&gt; contained in this wrapper.
+     * Get the List&lt;SignedIdentifier&gt; contained in this wrapper.
      *
-     * @return the List&lt;FileSignedIdentifier&gt;.
+     * @return the List&lt;SignedIdentifier&gt;.
      */
-    public List<FileSignedIdentifier> items() {
+    public List<SignedIdentifier> items() {
         return signedIdentifiers;
     }
 }
