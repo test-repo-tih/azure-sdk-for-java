a9f601c9009098452aaca3cf41ea8e24f7dcbb5b
Sameeksha Vaity <savaity@microsoft.com>
2019-10-14 22:23:32 -0700

    Azure-core API feedback (#5827)

--- sdk/storage/azure-storage-blob-batch/src/main/java/com/azure/storage/blob/batch/BlobBatchAsyncClient.java
+++ sdk/storage/azure-storage-blob-batch/src/main/java/com/azure/storage/blob/batch/BlobBatchAsyncClient.java
@@ -15,12 +15,11 @@ import com.azure.core.http.rest.Response;
 import com.azure.core.implementation.util.FluxUtil;
 import com.azure.core.util.Context;
 import com.azure.core.util.logging.ClientLogger;
-import com.azure.storage.blob.BlobServiceVersion;
 import com.azure.storage.blob.implementation.AzureBlobStorageBuilder;
 import com.azure.storage.blob.implementation.AzureBlobStorageImpl;
 import com.azure.storage.blob.models.AccessTier;
 import com.azure.storage.blob.models.DeleteSnapshotsOptionType;
-import com.azure.storage.blob.models.BlobStorageException;
+import com.azure.storage.blob.models.StorageException;
 import com.azure.storage.common.Utility;
 import reactor.core.publisher.Mono;
 
@@ -29,8 +28,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiFunction;
 
-import static com.azure.core.implementation.util.FluxUtil.monoError;
-import static com.azure.core.implementation.util.FluxUtil.pagedFluxError;
 import static com.azure.core.implementation.util.FluxUtil.withContext;
 
 /**
@@ -48,11 +45,10 @@ public final class BlobBatchAsyncClient {
 
     private final AzureBlobStorageImpl client;
 
-    BlobBatchAsyncClient(String accountUrl, HttpPipeline pipeline, BlobServiceVersion version) {
+    BlobBatchAsyncClient(String accountUrl, HttpPipeline pipeline) {
         this.client = new AzureBlobStorageBuilder()
             .url(accountUrl)
             .pipeline(pipeline)
-            .version(version.getVersion())
             .build();
     }
 
@@ -68,7 +64,7 @@ public final class BlobBatchAsyncClient {
     /**
      * Submits a batch operation.
      *
-     * <p>If any request in a batch fails this will throw a {@link BlobStorageException}.</p>
+     * <p>If any request in a batch fails this will throw a {@link StorageException}.</p>
      *
      * <p><strong>Code samples</strong></p>
      *
@@ -76,21 +72,17 @@ public final class BlobBatchAsyncClient {
      *
      * @param batch Batch to submit.
      * @return An empty response indicating that the batch operation has completed.
-     * @throws BlobStorageException If any request in the {@link BlobBatch} failed or the batch request is malformed.
+     * @throws StorageException If any request in the {@link BlobBatch} failed or the batch request is malformed.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Mono<Void> submitBatch(BlobBatch batch) {
-        try {
-            return withContext(context -> submitBatchWithResponse(batch, true, context)).flatMap(FluxUtil::toMono);
-        } catch (RuntimeException ex) {
-            return monoError(logger, ex);
-        }
+        return withContext(context -> submitBatchWithResponse(batch, true, context)).flatMap(FluxUtil::toMono);
     }
 
     /**
      * Submits a batch operation.
      *
-     * <p>If {@code throwOnAnyFailure} is {@code true} a {@link BlobStorageException} will be thrown if any request
+     * <p>If {@code throwOnAnyFailure} is {@code true} a {@link StorageException} will be thrown if any request
      * fails.</p>
      *
      * <p><strong>Code samples</strong></p>
@@ -101,16 +93,12 @@ public final class BlobBatchAsyncClient {
      * @param throwOnAnyFailure Flag to indicate if an exception should be thrown if any request in the batch fails.
      * @return A response only containing header and status code information, used to indicate that the batch operation
      * has completed.
-     * @throws BlobStorageException If {@code throwOnAnyFailure} is {@code true} and any request in the
-     * {@link BlobBatch} failed or the batch request is malformed.
+     * @throws StorageException If {@code throwOnAnyFailure} is {@code true} and any request in the {@link BlobBatch}
+     * failed or the batch request is malformed.
      */
     @ServiceMethod(returns = ReturnType.SINGLE)
     public Mono<Response<Void>> submitBatchWithResponse(BlobBatch batch, boolean throwOnAnyFailure) {
-        try {
-            return withContext(context -> submitBatchWithResponse(batch, throwOnAnyFailure, context));
-        } catch (RuntimeException ex) {
-            return monoError(logger, ex);
-        }
+        return withContext(context -> submitBatchWithResponse(batch, throwOnAnyFailure, context));
     }
 
     Mono<Response<Void>> submitBatchWithResponse(BlobBatch batch, boolean throwOnAnyFailure, Context context) {
@@ -129,16 +117,11 @@ public final class BlobBatchAsyncClient {
      * @param blobUrls Urls of the blobs to delete.
      * @param deleteOptions The deletion option for all blobs.
      * @return The status of each delete operation.
-     * @throws BlobStorageException If any of the delete operations fail or the request is malformed.
+     * @throws StorageException If any of the delete operations fail or the request is malformed.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     public PagedFlux<Response<Void>> deleteBlobs(List<String> blobUrls, DeleteSnapshotsOptionType deleteOptions) {
-        try {
-            return new PagedFlux<>(
-                () -> withContext(context -> submitDeleteBlobsBatch(blobUrls, deleteOptions, context)));
-        } catch (RuntimeException ex) {
-            return pagedFluxError(logger, ex);
-        }
+        return new PagedFlux<>(() -> withContext(context -> submitDeleteBlobsBatch(blobUrls, deleteOptions, context)));
     }
 
     PagedFlux<Response<Void>> deleteBlobsWithTimeout(List<String> blobUrls, DeleteSnapshotsOptionType deleteOptions,
@@ -162,15 +145,11 @@ public final class BlobBatchAsyncClient {
      * @param blobUrls Urls of the blobs to set their access tier.
      * @param accessTier {@link AccessTier} to set on each blob.
      * @return The status of each set tier operation.
-     * @throws BlobStorageException If any of the set tier operations fail or the request is malformed.
+     * @throws StorageException If any of the set tier operations fail or the request is malformed.
      */
     @ServiceMethod(returns = ReturnType.COLLECTION)
     public PagedFlux<Response<Void>> setBlobsAccessTier(List<String> blobUrls, AccessTier accessTier) {
-        try {
-            return new PagedFlux<>(() -> withContext(context -> submitSetTierBatch(blobUrls, accessTier, context)));
-        } catch (RuntimeException ex) {
-            return pagedFluxError(logger, ex);
-        }
+        return new PagedFlux<>(() -> withContext(context -> submitSetTierBatch(blobUrls, accessTier, context)));
         //return batchingHelper(blobUrls, (batch, blobUrl) -> batch.setTier(blobUrl, accessTier));
     }
 
