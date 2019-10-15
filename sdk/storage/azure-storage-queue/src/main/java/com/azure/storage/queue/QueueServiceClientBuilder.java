1626eeac7f5219ec78ec0e3896268249e48dffe7
Sameeksha Vaity <savaity@microsoft.com>
2019-10-14 22:23:32 -0700

    Azure-core API feedback (#5827)

--- sdk/storage/azure-storage-queue/src/main/java/com/azure/storage/queue/QueueServiceClientBuilder.java
+++ sdk/storage/azure-storage-queue/src/main/java/com/azure/storage/queue/QueueServiceClientBuilder.java
@@ -86,7 +86,6 @@ public final class QueueServiceClientBuilder {
     private HttpPipeline httpPipeline;
 
     private Configuration configuration;
-    private QueueServiceVersion version;
 
     /**
      * Creates a builder instance that is able to configure and construct {@link QueueServiceClient QueueServiceClients}
@@ -96,7 +95,6 @@ public final class QueueServiceClientBuilder {
     }
 
     private AzureQueueStorageImpl constructImpl() {
-        QueueServiceVersion serviceVersion = version != null ? version : QueueServiceVersion.getLatest();
         HttpPipeline pipeline = (httpPipeline != null) ? httpPipeline : BuilderHelper.buildPipeline(() -> {
             if (storageSharedKeyCredential != null) {
                 return new StorageSharedKeyCredentialPolicy(storageSharedKeyCredential);
@@ -107,12 +105,11 @@ public final class QueueServiceClientBuilder {
             } else {
                 return null;
             }
-        }, retryOptions, logOptions, httpClient, additionalPolicies, configuration, serviceVersion);
+        }, retryOptions, logOptions, httpClient, additionalPolicies, configuration);
 
         return new AzureQueueStorageBuilder()
             .url(endpoint)
             .pipeline(pipeline)
-            .version(serviceVersion.getVersion())
             .build();
     }
 
@@ -325,21 +322,6 @@ public final class QueueServiceClientBuilder {
         }
 
         this.httpPipeline = httpPipeline;
-        return this;
-    }
-
-    /**
-     * Sets the {@link QueueServiceVersion} that is used when making API requests.
-     * <p>
-     * If a service version is not provided, the service version that will be used will be the latest known service
-     * version based on the version of the client library being used. If no service version is specified, updating to a
-     * newer version the client library will have the result of potentially moving to a newer service version.
-     *
-     * @param version {@link QueueServiceVersion} of the service to be used when making requests.
-     * @return the updated QueueServiceClientBuilder object
-     */
-    public QueueServiceClientBuilder serviceVersion(QueueServiceVersion version) {
-        this.version = version;
         return this;
     }
 }
